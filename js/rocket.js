22c196f9859deef605b7424febe17fcfe0654703
ztatk <37038614+ztatk@users.noreply.github.com>
2018-04-07 14:52:03 +0200

    awesome

--- js/rocket.js
+++ js/rocket.js
@@ -11,13 +11,13 @@ var changeState = function (state) {
 	if (state == 2) {
 		timer = setInterval(function() {
 			countdownNumber = countdownNumber - 1;
-			
+
 			document.getElementById('countdown').innerHTML = countdownNumber;
 
 			if (countdownNumber <= 0) {
 			changeState(3)
 			};
-		}, 500);	
+		}, 200);	
 	}
 	else if (state == 3) {
 		var success = setTimeout(function ()
@@ -26,7 +26,7 @@ var changeState = function (state) {
 
 			console.log('randomNumber:' , randomNumber)
 
-			if (randomNumber > 5) {
+			if (randomNumber > 9) {
 				changeState(4)
 			}
 			else {
